a309e50d967c1d61c511832879193f54382f486b
idogwu chinonso <idogwuchinonso1@gmail.com>
2023-02-14 16:55:17 +0100

    minor changes to a few files

--- scripts/votingtoken.ts
+++ scripts/votingtoken.ts
@@ -6,7 +6,7 @@ async function main() {
   //const CloneMultiSig = await ethers.getContractFactory("votingToken");
   const VotingToken = await ethers.getContractFactory("votingToken");
   //console.log(await ethers.getContractFactory("cloneMultiSig"));
-  const votingToken = await VotingToken.deploy(1000000000000, "NONSO INU", "NNS");
+  const votingToken = await VotingToken.deploy(100000000000, "NONSO INU", "NNS");
   await votingToken.deployed();
   
   console.log(`VotingToken Address is ${votingToken.address}`);
@@ -19,21 +19,21 @@ const purchaseToken = await votingToken.PurchaseToken({value: ethers.utils.parse
 //console.log(purchaseToken);
 //await purchaseToken.wait();
 
-const createPool = await votingToken.createVotePool2("0x13B109506Ab1b120C82D0d342c5E64401a5B6381","0xBB9F947cB5b21292DE59EFB0b1e158e90859dddb", "0xfd182E53C17BD167ABa87592C5ef6414D25bb9B4");
+const createPool = await votingToken.createVotePool2(1000,"0x13B109506Ab1b120C82D0d342c5E64401a5B6381","0xBB9F947cB5b21292DE59EFB0b1e158e90859dddb", "0xfd182E53C17BD167ABa87592C5ef6414D25bb9B4");
 let event = await createPool.wait();
 //console.log(event);
-let child = await event.events[1].args;
-let voteId = child[0];
-console.log(`VOTE POOL ID IS ${voteId}`);
-const contenders = votingToken.DisplayContenders(voteId);
+//let child = await event.events[1].args;
+//let voteId = child[0];
+//console.log(`VOTE POOL ID IS ${voteId}`);
+const contenders = votingToken.DisplayContenders(1000);
 const contenders2 = await contenders;
 console.log(await contenders2);
-const vote = await votingToken.Vote(voteId, "0xfd182E53C17BD167ABa87592C5ef6414D25bb9B4", "0xBB9F947cB5b21292DE59EFB0b1e158e90859dddb", "0x13B109506Ab1b120C82D0d342c5E64401a5B6381");
+const vote = await votingToken.Vote(1000, "0xfd182E53C17BD167ABa87592C5ef6414D25bb9B4", "0xBB9F947cB5b21292DE59EFB0b1e158e90859dddb", "0x13B109506Ab1b120C82D0d342c5E64401a5B6381");
 
 // close voting pool
-await votingToken.closeVotingPool(voteId);
-const closePool = votingToken.closeVotingPool(voteId);
-const win = await votingToken.DisplayWinner(voteId);
+await votingToken.closeVotingPool(1000);
+const closePool = votingToken.closeVotingPool(1000);
+const win = await votingToken.DisplayWinner(1000);
 const winner = await win;
 console.log(winner);
 //await votingToken.vote(voteId, "0xfd182E53C17BD167ABa87592C5ef6414D25bb9B4", "0xBB9F947cB5b21292DE59EFB0b1e158e90859dddb", "0x13B109506Ab1b120C82D0d342c5E64401a5B6381")
